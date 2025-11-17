dc0a4e2a73cdf133ba2dddbe41e1222a2ae25ae4
tomori <tomori.gypsophila@foxmail.com>
2025-11-17 14:01:48 +0800

    common

---
+++ exp1/data_type.py
@@ -0,0 +1,13 @@
+print("I\'m OK")
+print("I\'m learning\nPython")
+print(r'\\\t\\\\\\')
+print('''line1
+line2
+line3''')
+
+a=123
+print(a)
+b = 'ABC'
+print(b)
+PI=3.1415926535897923354626
+print(PI)
