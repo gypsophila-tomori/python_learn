5a7acac04ebedcf4639c3c5b5558c80269e73a50
tomori <tomori.gypsophila@foxmail.com>
2025-11-19 14:10:50 +0800

    new file

--- exp1/cycle.py
+++ exp1/cycle.py
@@ -6,3 +6,30 @@ print(sum)
 names = ['Tomori','Gypsophila','Mike','Tom']
 for name in names:
     print(name)
+
+sum1 = 0
+for a in range(101):
+    sum1 = sum1 + a
+print(sum1)
+
+sum2 = 0
+n = 99
+while n > 0:
+    sum2 = sum2 +n
+    n = n - 2
+print(sum2)
+
+L = ['Bart','Lisa','Adma']
+count = 0
+while count <= 2:
+    print(L[count])
+    count = count + 1
+print("===========I am a line============\n")
+
+n = 1
+while n <= 100:
+    if n > 10:
+        break
+    print(n,"\n")
+    n = n + 1
+
