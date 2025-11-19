4c2348665327c036e0c4030881198533ba809b2c
tomori <tomori.gypsophila@foxmail.com>
2025-11-19 15:35:52 +0800

    new test

---
+++ exp6/function3.py
@@ -0,0 +1,3 @@
+from my_fun import power
+a = power(3, 2)
+print(a)
--- exp6/my_fun.py
+++ exp6/my_fun.py
@@ -14,3 +14,10 @@ def move(x, y, step, angle=0):
     nx = x + step * math.cos(angle)
     ny = y -step * math.sin(angle)
     return nx, ny
+
+def power(x, n):
+    s = 1
+    while n > 0:
+        s = s * x
+        n = n - 1
+    return s
