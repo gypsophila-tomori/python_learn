e0344a60753e7649d8a2784c3a1c8429ef44a705
tomori <tomori.gypsophila@foxmail.com>
2025-11-19 14:59:24 +0800

    new file

--- exp6/function1.py
+++ exp6/function1.py
@@ -3,3 +3,8 @@ print(abs(-20))
 c = max(12, 20, -5, 30)
 print(c)
 print("============I am a line============\n")
+
+print(int(12.34), float(12.34), str(12.3), bool(1), bool(''))
+#a = abs
+#print(a(-1))   别名
+print(hex(255))
